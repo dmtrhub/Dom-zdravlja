c5c3ce7a8ee06db5667d28a3c188fd96e8ac34b4
Dimitrije Vukovic <dmtr.develop@gmail.com>
2024-07-15 21:44:20 +0200

    Initial admin model

---
+++ Models/Administrator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Dom_zdravlja.Models
+{
+    public class Administrator
+    {
+        string korisnickoIme;
+        int sifra;
+        string ime;
+        string prezime;
+        DateTime datumRodjenja;
+
+        public string KorisnickoIme { get => korisnickoIme; set => korisnickoIme = value; }
+
+        public int Sifra { get => sifra; set => sifra = value; }
+
+        public string Ime { get => ime; set => ime = value; }
+
+        public string Prezime { get => prezime; set => prezime = value; }
+
+        public DateTime DatumRodjenja { get => datumRodjenja; set => datumRodjenja = value; }
+    }
+}
