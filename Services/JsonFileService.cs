a381c41b2d9de490985a9b1682c649ff8373a31c
Dimitrije Vukovic <dmtr.develop@gmail.com>
2024-08-05 01:35:02 +0200

    Implemented service for reading data from json files

---
+++ Services/JsonFileService.cs
@@ -0,0 +1,39 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace Dom_zdravlja.Services
+{
+    public class JsonFileService<T>
+    {
+        private readonly string _filePath;
+
+        public JsonFileService(string filePath)
+        {
+            _filePath = filePath;
+        }
+
+        public List<T> GetAll()
+        {
+            if (!File.Exists(_filePath))
+            {
+                return new List<T>();
+            }
+
+            var json = File.ReadAllText(_filePath);
+            return JsonConvert.DeserializeObject<List<T>>(json);
+        }
+
+        public void Add(T item)
+        {
+            var items = GetAll();
+            items.Add(item);
+            var json = JsonConvert.SerializeObject(items, Formatting.Indented);
+            File.WriteAllText(_filePath, json);
+        }
+    }
+}
